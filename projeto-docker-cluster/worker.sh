3bcbbf3ac4a120b722004954dcb3c5faf8089b22
Thamyres Magalhães <pink.thamyres@gmail.com>
2022-09-26 11:25:20 -0300

    Add files via upload

---
+++ projeto-docker-cluster/worker.sh
@@ -0,0 +1,2 @@
+    docker swarm join --token SWMTKN-1-3pj8k0i4tn77bd93a0yxhgh36hxuef5q5oyg1732rztnfy29ll-a94q0ipwgrjs4xikzyb4yb3n5 10.10.10.100:2377
+#Arquivo que será utilizado para armazenar o token que não necessariamente será esse. 
